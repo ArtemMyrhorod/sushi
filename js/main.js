d29ee56e7532f493369b01956958e4c1cfcd7af4
Artem Myrhorod <mirgorodartem@gmail.com>
2022-11-27 19:04:59 +0100

    Finished Mobile Version

--- js/main.js
+++ js/main.js
@@ -13,3 +13,22 @@ if (navClose) {
       navMenu.classList.remove("show-menu");
    });
 }
+
+// Remove Menu in Mobile Version
+const navLink = document.querySelectorAll(".header__menu-link");
+
+const linkAction = () => {
+   const navMenu = document.getElementById("nav-menu");
+   navMenu.classList.remove("show-menu");
+};
+
+navLink.forEach(n => n.addEventListener("click", linkAction));
+
+// Change Background for Header
+
+const scrollHeader = () => {
+   const header = document.getElementById("header");
+   this.scrollY >= 50 ? header.classList.add("bg-header") : header.classList.remove("bg-header");
+};
+
+window.addEventListener("scroll", scrollHeader);
